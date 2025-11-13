94f5b2f18030f198d0bab2a2cff7b02b77686996
RojZ23 <rzalzar04@gmail.com>
2025-11-13 13:33:29 -0800

    changes pom

---
+++ src/main/java/com/example/labai/LabAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.labai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/labai/LabAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
